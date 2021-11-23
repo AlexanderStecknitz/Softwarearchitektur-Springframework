cb3c4d33c2edde5e1cb97f17e2216b4357ddb841
Alexander Stecknitz <alex.stecknitz@web.de>
2021-11-23 19:21:09 +0100

    try out changed api path

--- src/main/kotlin/rest/ArtikelGetController.kt
+++ src/main/kotlin/rest/ArtikelGetController.kt
@@ -90,7 +90,7 @@ class ArtikelGetController(private val service: ArtikelReadService) {
         /**
          * Konstante für den API-Path
          */
-        const val API_PATH = "/api"
+        const val API_PATH = "/api1"
         private val logger: Logger = LoggerFactory.getLogger(ArtikelGetController::class.java)
     }
 }
